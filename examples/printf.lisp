802ef9de7233c7aea84cc4ace42e10870959bd42
selat <selatnick@gmail.com>
2015-04-21 21:48:53 +0300

    Use find-symbol function to find symbol by it's string name

--- examples/printf.lisp
+++ examples/printf.lisp
@@ -6,7 +6,7 @@
     (loop for i = (position #\{ s) until (not i)
           for j = (position #\} s) when (or (not j) (< j i)) return (error "Mismatched { and }") do
             (setf format-s (concatenate 'string format-s (subseq s 0 i) "~A"))
-            (setf var-list (cons (make-symbol (subseq s (1+ i) j)) var-list))
+            (setf var-list (cons (find-symbol (string-upcase (subseq s (1+ i) j))) var-list))
             (setf s (subseq s (1+ j) (length s))))
     (setf format-s (concatenate 'string format-s s))
     `(format t ,format-s ,@var-list)))
